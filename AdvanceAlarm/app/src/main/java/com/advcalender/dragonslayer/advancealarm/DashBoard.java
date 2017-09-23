c4bf4b2fda6c276d69b32c45f99218eee813c828
Your Name <you@example.com>
2017-09-22 23:58:10 -0500

    Added start and stop without functionality

--- AdvanceAlarm/app/src/main/java/com/advcalender/dragonslayer/advancealarm/DashBoard.java
+++ AdvanceAlarm/app/src/main/java/com/advcalender/dragonslayer/advancealarm/DashBoard.java
@@ -1,7 +1,12 @@
 package com.advcalender.dragonslayer.advancealarm;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashBoard extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class DashBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        final Button checkNetwork = (Button) findViewById(R.id.checkNetwork);
+        checkNetwork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isDeviceOnline()) {
+                    checkNetwork.setText(R.string.connected);
+                }
+                else checkNetwork.setText(R.string.disconnected);
+            }
+        });
+    }
+
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
     }
 }
